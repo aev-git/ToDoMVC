c7b54899bd75d2be5fb250859ec9ab77795a99eb
ymvrali <aevilla11@gmail.com>
2017-10-29 13:15:08 -0400

    scaffolded a todo list framework

---
+++ WebApplication1/WebApplication1/Models/ToDoList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class ToDoList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public int ToDoListId { get; set; }
+        public string Memo { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
